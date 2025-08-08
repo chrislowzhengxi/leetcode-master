ccafcfd36d1f0b71badc5d14aba4a41cf7214039
Chris Low <chrislowzhengxi@uchicago.edu>
2025-08-08 10:28:02 +0800

    Added Akuna

---
+++ arrays/NA2-akuna-substring.py
@@ -0,0 +1,21 @@
+"""
+LeetCode NA2: Akuna Substring
+https://leetcode.com/problems/akuna-substring/
+
+📝 Summary:
+
+🧠 Thought Process:
+
+✅ Solution Approach:
+
+🐞 Mistakes & Gotchas:
+
+🎓 What I Learned:
+
+🔁 Revisit: No
+"""
+
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        # TODO: implement
+        pass
